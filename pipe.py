522de2a29592592bc383f32bf2970ee0f0ea9ed6
Janani <jenny.stone125@gmail.com>
2013-12-29 08:45:21 +0530

    Defining stages as classes

--- pipe.py
+++ pipe.py
@@ -1,43 +1,62 @@
 #------------------------- pipe.py -------------------------
-'''
-   A simple Python implementation of pipe
-  "readfile c:\file1.txt | locate /apple/ | writefile c:\file2.txt"
-   The output of each stage is connected to the input of the next,
-   forming form a pipeline. This is a static configuration. Our
-   subsequent goal is a dynamic configuration constructed automatically
-   from the pipeline specification. 
-'''
-def readFile():
-'''
-   This method is the first stage and acts as an input driver.
-   An input driver repeatedly obtain a record from some source
-   and passes it to the next stage.
-   It is responsible for opening and closing its source.
-'''
-  with open("c:/file1.txt", "r") as f: #If on Linux, substitute a Linux path for c:\ 
-    for record in f:
-      locate(record)
-    locate() # pass EOF indicator
 
-def locate(record=None):
+# Define the stage classes
+
+class ReadFile():
+  
+  def __init__(self, sourceFileName):
 '''
-   locate is a filter stage or filter.
-   A filter selects records for output based on some criteria.
+   Stage instances are initialized by passing the stage
+   specification (spec) to the __init__ method.
+   __init__ may do nothing, save the spec, or process in
+   some meaningful manner.
 '''   
-  if record is None or 'apple' in record:
-    writeFile(record)
+    self.sourceFileName = sourceFileName
 
-def writeFile(record, destFile=open("c:/file2.txt", "w")): #If on Linux, substitute a Linux path for c:\ 
+  def run(self):
 '''
-   The final stage is an output driver. An output driver
-   sends records to an external destination e.g. file, terminal,
-   database, tcp/ip connection. It is responsible for opening
-   and closing its destination.
-'''   
-  if record is None: # EOF
-    destFile.close()
-  else:
-    destFile.write(record)
+ each stage class has a run method.The run method of the
+ input driver is called once to start that stage.
+'''
+    with open(self.sourceFileName, "r") as f:
+      for record in f:
+        self.output(record) # each stage class has an output method
+      self.output()
 
-readFile()
+class Locate:
+
+  def __init__(self, locateStr):
+    self.locateStr = locateStr
+
+  def run(self,record=None):
+    if record is None or record.find(self.locateStr) >= 0:
+      self.output(record)
+
+class WriteFile:
+  
+  def __init__(self, destFileName):
+    self. destFile = open(destFileName, "w")
+  def run(self, record):    
+    if record is None:
+      self. destFile.close()
+    else:
+      self.destFile.write(record)
+
+# Create stage instances passing the stage specification to __init__.
+# This is the pipeline specification we are applying:
+# "readfile c:\file1.txt | locate /apple/ | writefile c:\file2.txt"
+
+stage1 = ReadFile("c:/file1.txt")
+stage2 = Locate("apple")
+stage3 = WriteFile("c:/file2.txt")
+
+# connect the output method  of each stage to the run method of the next.
+
+stage1.output = stage2.run
+stage2.output = stage3.run
+
+# start the pipeline
+
+stage1.run()
+
 #------------------------- end pipe.py -------------------------
